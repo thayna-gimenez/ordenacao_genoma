8ef2959f2efe308a52c5805738ec58d612f0e2fb
gabriel-meireles016 <meirelesgabriel016@gmail.com>
2025-09-12 15:02:23 -0400

    Criação do gerador de sequências de DNA

---
+++ src/gerar_exemplos.c
@@ -0,0 +1,51 @@
+// gcc -o gerar_exemplos /src/gerar_exemplos.c
+// ./gerar_exemplos 100000 testes/entradas/ex_pequeno.txt
+// ./gerar_exemplos 1000000 testes/entradas/ex_medio.txt
+// ./gerar_exemplos 10000000 testes/entradas/ex_grande.txt
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#define MAX_SEQ_LENGTH 100
+#define DNA_CHARS "ACGT"
+
+void generate_dna_sequence(char* seq, int length) {
+    for (int i = 0; i < length; i++) {
+        seq[i] = DNA_CHARS[rand() % 4];
+    }
+    seq[length] = '\0';
+}
+
+int main(int argc, char *argv[]) {
+    
+    if (argc != 3) {
+        printf("Comando errado.");
+        return 1;
+    }
+    
+    int num_seqs = atoi(argv[1]);
+    
+    // Geração aleatória de keys com base na hora
+    srand(time(0));
+    
+    // Abertura de arquivo txt
+    FILE* arquivo = fopen(argv[2], "w");
+    if (!arquivo) {
+        printf("Erro ao criar o arquivo txt.");
+        return 1;
+    }
+    
+    for (int i = 0; i < num_seqs; i++) {
+        int tamanho = 10 + (rand() % 91); // gera valores de 10 a 100
+        char sequencia[MAX_SEQ_LENGTH + 1];
+        generate_dna_sequence(sequencia, tamanho);
+        fprintf(arquivo, "%s\n", sequencia);
+    }
+    
+
+    fclose(arquivo);
+    return 0;
+}
